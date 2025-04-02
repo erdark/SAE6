c3c50ea46b26b18c9653687b0726db27e836414e
Vladimir <vladimir.rekai@etu.unicaen.fr>
2025-04-02 11:45:54 +0200

    correct mock data and add routing

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
-header("Location: pages/home.php");
-exit;
+        if (isset($_GET['page'])) {
 
-?>
+            if ($_GET['page'] == 'add') {
+                require_once("pages/add_perturbation.php");
+            }
+        }
+
+    ?>
+</body>
+</html>
+
---
+++ pages/add_perturbation.php
@@ -0,0 +1,1 @@
+<p>samarche</p>
