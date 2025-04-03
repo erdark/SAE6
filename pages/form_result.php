89f3e8aae8e55e629952a7c6a0748383e3450c21
Vladimir_rekai <vladimir.rekai.etu.unicaen.fr>
2025-04-03 09:21:24 +0200

    feat: adapt form handling for other types

--- pages/form_result.php
+++ pages/form_result.php
@@ -2,18 +2,40 @@
 
 require_once '../session.php';
 
-
 foreach ($_SESSION['data']->pistes as $piste){
     if($piste->id == $_GET['id']){
-        $piste_concernee = $piste;
+        $installation_concernee = $piste;
     }
 }
-var_dump($piste_concernee);
+if(!isset($installation_concernee)){
+    foreach ($_SESSION['data']->parkings as $parking){
+        if($parking->id == $_GET['id']){
+            $installation_concernee = $parking;
+        }
+    }
+}
+
+if(!isset($installation_concernee)){
+    foreach ($_SESSION['data']->restaurants as $restaurant){
+        if($restaurant->id == $_GET['id']){
+            $installation_concernee = $restaurant;
+        }
+    }
+}
+if(!isset($installation_concernee)){
+    foreach ($_SESSION['data']->remontees as $remontee){
+        if($remontee->id == $_GET['id']){
+            $installation_concernee = $remontee;
+        }
+    }
+}
+
+var_dump($installation_concernee);
 echo "<br/><br/>";
 
-array_push($piste_concernee->perturbations, (object) array('start' => $_POST['start'], 'end' => $_POST['end'], 'description' => $_POST['description']));
+array_push($installation_concernee->perturbations, (object) array('start' => $_POST['start'], 'end' => $_POST['end'], 'description' => $_POST['description']));
 
-var_dump($piste_concernee);
+var_dump($installation_concernee);
 echo "<br/><br/>";
 var_dump($_SESSION);
 // header("Location: home.php");
